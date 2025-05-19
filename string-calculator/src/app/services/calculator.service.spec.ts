2fd08d93007e335655abeead00b01dd5bd4953e9
Mohammad Akram <aakram1708@gmail.com>
2025-05-19 20:54:06 +0530

    :apple: :test_tube: added test cases for calculator service

--- string-calculator/src/app/services/calculator.service.spec.ts
+++ string-calculator/src/app/services/calculator.service.spec.ts
@@ -6,11 +6,30 @@ describe('CalculatorService', () => {
   let service: CalculatorService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CalculatorService);
+    service = new CalculatorService();
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should return 0 for empty string', () => {
+    expect(service.add('')).toBe(0);
   });
+  it('should return number for single input', () => {
+    expect(service.add('5')).toBe(5);
+  });
+
+  it('should return sum for two numbers', () => {
+    expect(service.add('1,2')).toBe(3);
+  });
+
+  it('should handle newlines as delimiters', () => {
+    expect(service.add('1\n2,3')).toBe(6);
+  });
+
+  it('should support custom delimiters', () => {
+    expect(service.add('//;\n1;2')).toBe(3);
+  });
+
+  it('should throw for negative numbers', () => {
+    expect(() => service.add('1,-2,3,-4')).toThrowError('Negative numbers not allowed: -2, -4');
+  });
+
 });
